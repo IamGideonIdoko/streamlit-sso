655951030a5bb5dc278f8b4186ddffecd89e118f
Gideon Idoko <iamgideonidoko@gmail.com>
2024-06-21 22:10:43 +0100

    feat: impl sso

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from descope.descope_client import DescopeClient
 from descope.exceptions import AuthException
 
 DESCOPE_PROJECT_ID = str(st.secrets.get("DESCOPE_PROJECT_ID"))
+DESCOPE_TENANT_ID = str(st.secrets.get("DESCOPE_TENANT_ID"))
 descope_client = DescopeClient(project_id=DESCOPE_PROJECT_ID)
 
 
@@ -14,7 +15,6 @@ if "token" not in st.session_state:
             # Exchange code
             with st.spinner("Loading..."):
                 jwt_response = descope_client.sso.exchange_token(code)
-            st.write("SSO Exchange successful")
             st.session_state["token"] = jwt_response["sessionToken"].get("jwt")
             st.session_state["refresh_token"] = jwt_response["refreshSessionToken"].get(
                 "jwt"
@@ -25,21 +25,26 @@ if "token" not in st.session_state:
             st.error("Login failed!")
     st.warning("You're not logged in, pls login")
     with st.container(border=True):
-        with st.form(key="sso_form", clear_on_submit=True, border=False):
-            email = st.text_input("Email address", placeholder="john@doe.com")
-            sso_button = st.form_submit_button(
-                "Sign in with SSO", use_container_width=True
-            )
-            st.write(
-                '<p style="text-align:center">------- OR -------</p>',
-                unsafe_allow_html=True,
-            )
         if st.button("Sign In with Google", use_container_width=True):
             oauth_response = descope_client.oauth.start(
                 provider="google", return_url="http://localhost:8501"
             )
             url = oauth_response["url"]
             # Redirect to Google
+            st.markdown(
+                f'<meta http-equiv="refresh" content="0; url={url}">',
+                unsafe_allow_html=True,
+            )
+        st.write(
+            '<p style="text-align:center">------- OR -------</p>',
+            unsafe_allow_html=True,
+        )
+        if st.button("Sign in with SSO", use_container_width=True):
+            sso_response = descope_client.sso.start(
+                tenant=DESCOPE_TENANT_ID, return_url="http://localhost:8501"
+            )
+            url = sso_response["url"]
+            # Redirect to Okta
             st.markdown(
                 f'<meta http-equiv="refresh" content="0; url={url}">',
                 unsafe_allow_html=True,
@@ -57,7 +62,7 @@ else:
         if "user" in st.session_state:
             user = st.session_state.user
             st.write("Name: " + user["name"])
-            st.write("email: " + user["email"])
+            st.write("Email: " + user["email"])
         if st.button("Logout"):
             del st.session_state.token
             st.rerun()
